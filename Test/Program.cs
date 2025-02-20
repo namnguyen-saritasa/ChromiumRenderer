ee3bdbea46ae05a4151d1be50c3927b0f90e42ef
Nam Nguyen <nam.nguyen@saritasa.com>
2025-02-20 16:07:32 +0700

    Fixed Windows building pipelineˆ

--- Test/Program.cs
+++ Test/Program.cs
@@ -7,20 +7,6 @@ class Program
 {
     public static async Task Main()
     {
-        {
-            Console.WriteLine("Downloading chrome");
-            var fetcher = await (new BrowserFetcher(SupportedBrowser.Chrome).DownloadAsync(BrowserTag.Stable));
-            var executablePath = fetcher.GetExecutablePath();
-            Console.WriteLine($"Headless chrome path: {executablePath}");
-            Console.WriteLine("Launching");
-            using var browser = await Puppeteer.LaunchAsync(new()
-            {
-                Headless = true,
-                ExecutablePath = executablePath
-            });
-            Console.WriteLine("New page");
-            using var page = await browser.NewPageAsync();
-        }
         Console.WriteLine("Initializing renderer");
         await using var renderer = new HtmlRenderer();
         Console.WriteLine("Rendering");
